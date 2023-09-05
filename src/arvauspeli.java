12db046c2bdf49d266e9307720907fefc75576a0
kaisupaukkeri <kaisu.paukkeri@student.hamk.fi>
2023-09-05 20:07:08 +0300

    Ohjelman aloitus
    Aloitin nimen arvauspelin koodaamisen alustamalla muuttujat ja kysymällä nimet do-while -silmukalla

--- src/arvauspeli.java
+++ src/arvauspeli.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class arvauspeli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		// Luodaan muuttujat
+		String name = "Kaisu";
+		String answer = "";
+		
+		do
+		{
+			System.out.println("Guess my name: ");
+			answer = in.nextLine();
+			
+			// Muutetaan nimet pieniksi kirjaimiksi, niin arvailu on helpompaa
+			name = name.toLowerCase();
+			answer = answer.toLowerCase(); 
+		
+		} while (!name.equals(answer));
+		
+		
 
 	}
 
